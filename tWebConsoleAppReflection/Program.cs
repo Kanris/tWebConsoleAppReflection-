020fef07ced83e2022d22cbdc17b1314eb9049d1
Alexey Glukhov <kanris2@gmail.com>
2019-03-07 14:09:38 +0200

    Initial commit

---
+++ tWebConsoleAppReflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace tWebConsoleAppReflection
+{
+    class Program
+    {
+        private const string url = "http://localhost:9000";
+
+        private static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Server started at:" + url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ tWebConsoleAppReflection/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace tWebConsoleAppReflection
+{
+    //check in http://localhost:9000/api/test/GetTestControllerMethods
+
+    public class TestController : ApiController
+    {
+        //test method
+        public IEnumerable<string> GetTest()
+        {
+            return new string[] { "One", "Two", "Three" };
+        }
+
+        //display all methods in classes within tWebConsoleAppReflection namespace
+        public IEnumerable<string> GetTestControllerMethods()
+        {
+            /*var methodsInfo = typeof(TestController)
+                .GetMethods().Where(x => x.IsFamily);*/
+
+            var resultList = new List<string>();
+
+            foreach (var typeInNamespace in 
+                                GetTypesInNamespace(Assembly.GetExecutingAssembly(), "tWebConsoleAppReflection"))
+            {
+                var methodsInfo = typeInNamespace.GetMethods();
+                resultList.Add($"{new string('-', 5)} {typeInNamespace.Name} {new string('-', 5)}");
+
+                foreach (var methodInfo in methodsInfo)
+                    resultList.Add(methodInfo.Name);
+            }
+
+            return resultList;
+        }
+
+        //return all types in current namespace
+        private Type[] GetTypesInNamespace(Assembly assembly, string nameSpace)
+        {
+            return
+              assembly.GetTypes()
+                      .Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal))
+                      .ToArray();
+        }
+    }
+}
